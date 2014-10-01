7c6eb13485621c55e46d0e30197a5c3965b278bc
Jordan M. Adler <jmadler@cpan.org>
2014-10-01 18:19:34 +0200

    Move brackets into a proper data structure, and, in the process, remove a bug where we tax the income below our bracket twice (once by virtue of the base, once by virtue of the agi times current bracket rate).

--- lib/nyctax.py
+++ lib/nyctax.py
@@ -4,60 +4,62 @@ def tax_calc(gross, deduction, residence):
 
     tax_total = 0
 
-    if residence == 'NYC':
+    city_tax_bracket = [
         # http://www.tax.ny.gov/pdf/current_forms/it/nyc_tax_rate_schedule.pdf
-        if gross > 500000:
-            tax_total += 18122 + (gross * 0.03876)
-        else if gross > 50000:
-            tax_total += 1706 + (gross * 0.03648)
-        else if gross > 25000:
-            tax_total += 808 + (gross * 0.03591)
-        else if gross > 12000:
-            tax_total += 349 + (gross * 0.03534)
-        else:
-            tax_total += 0 + (gross * 0.02907)
+        { 'over': 500000, 'base': 18122, 'rate': 0.03876 },
+        { 'over': 50000, 'base': 1706, 'rate': 0.03648 },
+        { 'over': 25000, 'base': 808, 'rate': 0.03591 },
+        { 'over': 12000, 'base': 349, 'rate': 0.03534 },
+        { 'over': 0, 'base': 349, 'rate': 0.03534 },
+    ]
+
+    if residence == 'NYC':
+        for i in city_tax_bracket:
+            if gross > i['over']:
+                tax_total += i['base']
+                tax_total += i['rate'] * ( gross - i['over'] )
+                break
     else:
         # We don't yet support other areas.
         raise Exception
 
-    # state 
-    # http://www.tax.ny.gov/pdf/2013/inc/it201i_2013.pdf
-    if gross > 1029250:
-        tax_total += 69612 + (gross * 0.0882)
-    else if gross > 205850:
-        tax_total += 13209 + (gross * 0.0685)
-    else if gross > 77150:
-        tax_total += 4651 + (gross * 0.0665)
-    else if gross > 20550:
-        tax_total += 1000 + (gross * 0.0645)
-    else if gross > 13350:
-        tax_total += 575 + (gross * 0.059)
-    else if gross > 11300:
-        tax_total += 468 + (gross * 0.0525)
-    else if gross > 8200:
-        tax_total += 328 + (gross * 0.045)
-    else:
-        tax_total += 0 + (gross * 0.04)
-        
-    # federal
-    # where AGI = income - state tax - city tax - standard deduction (5,800) - all other deductions (0)
-    # http://www.irs.gov/pub/irs-pdf/i1040.pdf 
-    # http://www.irs.gov/publications/p501/index.html
+    state_tax_bracket = [
+        # http://www.tax.ny.gov/pdf/current_forms/it/nyc_tax_rate_schedule.pdf
+        { 'over': 1029250, 'base': 69612, 'rate': 0.0882 },
+        { 'over': 205850, 'base': 13209, 'rate': 0.0685 },
+        { 'over': 77150, 'base': 4651, 'rate': 0.0665 },
+        { 'over': 20550, 'base': 1000, 'rate': 0.059 },
+        { 'over': 11300, 'base': 468, 'rate': 0.0525 },
+        { 'over': 8200, 'base': 328, 'rate': 0.045 },
+        { 'over': 0, 'base': 0, 'rate': 0.04 },
+    ]
+
+    for i in state_tax_bracket:
+        if gross > i['over']:
+            tax_total += i['base']
+            tax_total += i['rate'] * ( gross - i['over'] )
+            break
+
+    federal_tax_bracket = [
+        # where AGI = income - state tax - city tax - standard deduction (5,800) - all other deductions (0)
+        # http://www.irs.gov/pub/irs-pdf/i1040.pdf 
+        # http://www.irs.gov/publications/p501/index.html
+        { 'over': 400000, 'base': 116163.75, 'rate': 0.396 },
+        { 'over': 398350, 'base': 115586.25, 'rate': 0.35 },
+        { 'over': 183250, 'base': 44603.25, 'rate': 0.33 },
+        { 'over': 87850, 'base': 17891.25, 'rate': 0.28 },
+        { 'over': 36250, 'base': 4991.25, 'rate': 0.25 },
+        { 'over': 8925, 'base': 892.50, 'rate': 0.15 },
+        { 'over': 0, 'base': 0, 'rate': 0.10 },
+    ]
+
     agi = gross - tax_total - deduction
-    if agi > 400000:
-        tax_total += 116163.75 + (agi * 0.396)
-    else if agi > 398350:
-        tax_total += 115586.25 + (agi * 0.35)
-    else if agi > 183250:
-        tax_total += 44603.25 + (agi * 0.33)
-    else if agi > 87850:
-        tax_total += 17891.25 + (agi * 0.28)
-    else if agi > 36250:
-        tax_total += 4991.25 + (agi * 0.25)
-    else if agi > 8925:
-        tax_total += 892.50 + (agi * 0.15)
-    else:
-        tax_total += 0 + (agi * 0.10)
+
+    for i in federal_tax_bracket:
+        if agi > i['over']:
+            tax_total += i['base']
+            tax_total += i['rate'] * ( agi - i['over'] )
+            break
 
     # FICA 
     # 6.2% SSA (up to the wage base of 113700)
